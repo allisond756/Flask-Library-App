dcd36432ea0c961967529baddf37a70c08e4868b
Allison Daniel <allisond756@gmail.com>
2023-05-08 14:49:24 -0500

    flask library

---
+++ config.py
@@ -0,0 +1,17 @@
+import os 
+from dotenv import load_dotenv
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
+
+class Config():
+    """
+        Set configg variable for the flask app
+        using Enviroment variables where available.
+        Otherwise, create the config variable if not done already
+    """
+
+    FLASK_APP = os.getenv('FLASK_APP')
+    FLASK_ENV = os.getenv('FLASK_ENV')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'Buffalo buffalo Buffalo buffalo buffalo buffalo Buffalo buffalo'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLACHEMY_TRACK_NOTIFICATIONS = False
